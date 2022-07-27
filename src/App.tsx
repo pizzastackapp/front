9cf1e810c6b779320f9c5422b5be29d429c96c9b
Dmytro Batarin <dmytro@batarin.dev>
2022-07-27 22:35:15 +0200

    Install storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
 export const App = () => {
-  return (
-    <h1 className="font-bold text-3xl text-red-500">🍕 PizzaStack</h1>
-  );
-}
+  return <h1 className="font-bold text-3xl text-red-500">🍕 PizzaStack</h1>;
+};
---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,11 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { App } from '../App';
+
+export default {
+  title: 'Example/App',
+  component: App,
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = () => <App />;
+
+export const Primary = Template.bind({});
