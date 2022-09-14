5a9e836155a5796fccbb6f44c7d95628a5ecb90a
Dmytro Batarin <dmytro@batarin.dev>
2022-09-14 21:34:55 +0200

    Add ref and fullWidth to input

--- src/common/components/input/input.component.tsx
+++ src/common/components/input/input.component.tsx
@@ -1,46 +1,54 @@
 import clsx from 'clsx';
-import { ComponentProps, FC, useId } from 'react';
+import { ComponentProps, forwardRef, useId } from 'react';
 import { ReactComponent as ExclamationCircleSolid } from '@app/assets/icons/exclamation-circle-solid.svg';
 
 interface InputProps {
-  onChange: ComponentProps<'input'>['onChange'];
-  onBlur: ComponentProps<'input'>['onBlur'];
-  onFocus: ComponentProps<'input'>['onFocus'];
-  value: ComponentProps<'input'>['value'];
+  onChange?: ComponentProps<'input'>['onChange'];
+  onBlur?: ComponentProps<'input'>['onBlur'];
+  value?: ComponentProps<'input'>['value'];
+  name?: ComponentProps<'input'>['name'];
   label: string;
   placeholder: string;
   error?: string;
+  fullWidth?: boolean;
 }
 
-export const Input: FC<InputProps> = ({ label, error, ...props }) => {
-  const inputId = useId();
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, fullWidth, ...props }, ref) => {
+    const inputId = useId();
 
-  const inputClasses = clsx(
-    'border shadow-sm bg-white rounded-md px-3 py-2 text-sm placeholder-gray-400 mb-2 outline-none transition-all',
-    {
-      'border-gray-300': !error,
-      'text-red-900 border-red-300 focus:border-red-500': error,
-    }
-  );
+    const inputClasses = clsx(
+      'border shadow-sm bg-white rounded-md px-3 py-2 text-sm placeholder-gray-400 mb-2 outline-none transition-all',
+      {
+        'border-gray-300': !error,
+        'text-red-900 border-red-300 focus:border-red-500': error,
+        'w-full': fullWidth,
+      }
+    );
 
-  return (
-    <div>
-      <label
-        htmlFor={inputId}
-        className="block text-sm font-medium text-gray-900 mb-1"
-      >
-        {label}
-      </label>
-      <div className="relative inline-block">
-        <input id={inputId} className={inputClasses} {...props} />
-        {error && (
-          <ExclamationCircleSolid className="h-4 w-4 absolute right-3 top-2.75 child-path:fill-red-500" />
-        )}
+    const inputWrapperClasses = clsx('relative inline-block', {
+      'w-full': fullWidth,
+    });
+
+    return (
+      <div>
+        <label
+          htmlFor={inputId}
+          className="block text-sm font-medium text-gray-900 mb-1"
+        >
+          {label}
+        </label>
+        <div className={inputWrapperClasses}>
+          <input id={inputId} className={inputClasses} ref={ref} {...props} />
+          {error && (
+            <ExclamationCircleSolid className="h-4 w-4 absolute right-3 top-2.75 child-path:fill-red-500" />
+          )}
+        </div>
+        <span
+          className="block text-sm text-red-600"
+          dangerouslySetInnerHTML={{ __html: error || '&nbsp;' }}
+        />
       </div>
-      <span
-        className="block text-sm text-red-600"
-        dangerouslySetInnerHTML={{ __html: error || '&nbsp;' }}
-      />
-    </div>
-  );
-};
+    );
+  }
+);
