23b2bca42f8167e4e4018b397deafe8f9182aedd
Dmytro Batarin <dmytro@batarin.dev>
2022-09-27 23:22:48 +0300

    Basic config for cart part

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,11 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        1.25: '0.3125rem',
         2.75: '0.6875rem',
+        3.5: '0.875rem',
         7.5: '1.875rem',
+        15: '3.75rem',
         49: '12.25rem',
         112: '28rem',
       },
