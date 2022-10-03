0ab4540bf5b23cdd0536ae8d0ea0eb6d426faca8
Dmytro Batarin <dmytro@batarin.dev>
2022-10-03 19:47:11 +0300

    Extend input loading component

--- src/common/components/input-loading/input-loading.component.tsx
+++ src/common/components/input-loading/input-loading.component.tsx
@@ -1,12 +1,25 @@
+import clsx from 'clsx';
 import { FC } from 'react';
 
-interface InputLoadingProps {}
+export enum InputLoadingSize {
+  sm = 'sm',
+  base = 'base',
+}
 
-export const InputLoading: FC<InputLoadingProps> = () => {
+interface InputLoadingProps {
+  size?: keyof typeof InputLoadingSize;
+}
+
+export const InputLoading: FC<InputLoadingProps> = ({ size }) => {
+  const inputClasses = clsx('w-full bg-gray-200 rounded-full', {
+    'h-10': size === InputLoadingSize.base,
+    'h-6': size === InputLoadingSize.sm,
+  });
+
   return (
     <div>
       <div className="w-16 h-5 bg-gray-200 rounded-full mb-1" />
-      <div className="w-full h-10 bg-gray-200 rounded-full" />
+      <div className={inputClasses} />
     </div>
   );
 };
